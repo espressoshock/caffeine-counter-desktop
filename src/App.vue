e6b00b6185b1093dd1dd79b44dab60587640e62f
Vincenzo Buono <vincebshock@gmail.com>
2019-02-27 05:58:05 +0100

    Title-bar component template added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <pour-animation></pour-animation>
-    <coffee-counter v-bind:maxCoffee="20"></coffee-counter>
-    <increment-coffee-btn></increment-coffee-btn>
-    <bottom-bar></bottom-bar>
+    <title-bar></title-bar>
+    <div class="window-content">
+      <pour-animation></pour-animation>
+      <coffee-counter v-bind:maxCoffee="20"></coffee-counter>
+      <increment-coffee-btn></increment-coffee-btn>
+      <bottom-bar></bottom-bar>
+    </div>
+
   </div>
 </template>
 
@@ -13,6 +17,7 @@ import BottomBar from "../src/components/bottom-bar";
 import CoffeeCounter from "../src/components/coffee-counter";
 import IncrementCoffeeBtn from "../src/components/increment-coffee-btn";
 import PourAnimation from "../src/components/pour-animation";
+import TitleBar from '../src/components/title-bar'
 
 import { mapActions } from "vuex";
 import { mapGetters } from "vuex";
@@ -27,7 +32,8 @@ export default {
     "bottom-bar": BottomBar,
     "pour-animation": PourAnimation,
     "coffee-counter": CoffeeCounter,
-    "increment-coffee-btn": IncrementCoffeeBtn
+    "increment-coffee-btn": IncrementCoffeeBtn,
+    "title-bar": TitleBar
   },
   methods: {}
 };
@@ -47,8 +53,14 @@ body,
 #app {
   overflow: hidden;
   width: 413px;
-  height: 461px;
+  height: 496px;
   background: #4a475f;
   position: relative;
+}
+.window-content{
+  position: relative;
+  top: 35px;
+  width: 100%;
+  height: 461px;
 }
 </style>
---
+++ src/components/title-bar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="title-bar">
+    <span class="title"></span>
+    <span class="icons">
+      <span class="minimize-btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="11px"
+          height="1px"
+        >
+          <path
+            fill-rule="evenodd"
+            fill="rgb(255, 255, 255)"
+            d="M-0.000,-0.000 L11.000,-0.000 L11.000,1.000 L-0.000,1.000 L-0.000,-0.000 Z"
+          ></path>
+        </svg>
+      </span>
+      <span class="close-btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="11.78px"
+          height="11.84px"
+        >
+          <path
+            fill-rule="evenodd"
+            fill="rgb(255, 255, 255)"
+            d="M7.674,6.713 L12.259,11.323 C12.560,11.626 12.560,12.115 12.259,12.417 C12.109,12.568 11.913,12.644 11.715,12.644 C11.518,12.644 11.321,12.568 11.171,12.417 L6.586,7.807 L2.000,12.417 C1.850,12.568 1.653,12.644 1.456,12.644 C1.259,12.644 1.062,12.568 0.912,12.417 C0.612,12.115 0.612,11.626 0.912,11.323 L5.498,6.713 L0.912,2.103 C0.612,1.801 0.612,1.311 0.912,1.009 C1.213,0.707 1.699,0.707 2.000,1.009 L6.586,5.619 L11.171,1.009 C11.472,0.707 11.959,0.707 12.259,1.009 C12.560,1.311 12.560,1.801 12.259,2.103 L7.674,6.713 Z"
+          ></path>
+        </svg>
+      </span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  },
+  name: "TitleBar",
+  data() {
+    return {};
+  },
+  mounted: function() {}
+};
+</script>
+
+<style scoped>
+
+</style>
