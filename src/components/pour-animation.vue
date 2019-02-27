d534936f7c64a81e43436ebe1c7e16563738c768
Vincenzo Buono <vincebshock@gmail.com>
2019-02-27 03:27:43 +0100

    Pour animation rewritten

--- src/components/pour-animation.vue
+++ src/components/pour-animation.vue
@@ -1,52 +1,61 @@
 <template>
-  <div id="pour-animation">
-    <div class="espresso-group-head" ref="groupHead" style="opacity: 1"></div>
+  <div id='pour-animation'>
+    <div class='espresso-group-head' ref='groupHead' style='opacity: 1'></div>
     <div
-      v-if="coffeeWrapperRender"
-      class="espresso-cup-wrapper"
-      ref="espressoCupWrapper"
-      v-on:mousedown="dragstart($event)"
-      v-on:mouseup="dragend($event)"
-      v-on:mouseleave="dragend($event)"
-      v-on:mousemove="drag($event)"
-      style="opacity: 1"
+      v-if='coffeeWrapperRender'
+      class='espresso-cup-wrapper'
+      ref='espressoCupWrapper'
+      v-on:mousedown='dragstart($event)'
+      v-on:mouseup='dragend($event)'
+      v-on:mouseleave='dragend($event)'
+      v-on:mousemove='drag($event)'
+      style='opacity: 1'
     >
-      <div class="espresso-cup">
-            <div class="coffee-drip-wrapper">
-      <div class="coffee-drip-svg-wrapper">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          width="85.31px"
-          height="220.19px"
-        >
-          <defs>
-            <clipPath id="drip-clip">
-              <path id="drip-clip-path"
-                fill-rule="evenodd"
-                fill="rgb(255, 255, 255)"
-                d="M-0.000,183.016 L-0.000,0.016 L96.000,0.016 L96.000,183.016 L-0.000,183.016 ZM96.000,198.016 L96.000,217.016 L-0.000,217.016 L-0.000,198.016 L96.000,198.016 ZM96.000,273.016 L-0.000,273.016 L-0.000,232.016 L96.000,232.016 L96.000,273.016 Z"
+      <div class='espresso-cup'>
+        <div class='coffee-drip-wrapper'>
+          <div class='coffee-drip-svg-wrapper'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              xmlns:xlink='http://www.w3.org/1999/xlink'
+              width='85.31px'
+              height='220.19px'
+            >
+              <defs>
+                <clipPath id='drip-clip'>
+                  <path
+                    id='drip-clip-path'
+                    fill-rule='evenodd'
+                    fill='rgb(255, 255, 255)'
+                    d='M-0.000,183.016 L-0.000,0.016 L96.000,0.016 L96.000,183.016 L-0.000,183.016 ZM96.000,198.016 L96.000,217.016 L-0.000,217.016 L-0.000,198.016 L96.000,198.016 ZM96.000,273.016 L-0.000,273.016 L-0.000,232.016 L96.000,232.016 L96.000,273.016 Z'
+                  ></path>
+                </clipPath>
+              </defs>
+              <path
+                fill-rule='evenodd'
+                fill='rgb(192, 127, 69)'
+                d='M82.543,171.411 C70.045,195.000 45.000,191.813 45.000,191.813 C45.000,191.813 19.955,195.000 7.456,171.411 C5.427,167.581 3.586,163.748 1.893,159.938 C1.940,159.800 1.981,159.649 2.030,159.514 C2.020,159.489 2.011,159.463 2.002,159.440 C2.022,159.159 2.052,158.869 2.092,158.578 L2.092,151.566 C2.113,134.435 2.092,95.372 3.100,73.483 L3.100,73.373 C3.100,49.470 3.785,28.333 4.854,21.601 C5.570,17.114 7.864,13.607 11.000,10.859 L11.000,23.822 C10.052,31.532 9.147,51.143 9.147,73.112 L9.147,73.263 C8.145,97.794 8.149,140.076 8.139,157.446 C9.254,171.519 17.668,185.000 44.906,185.000 C45.559,185.000 46.172,185.012 46.768,185.029 C68.800,184.025 77.362,173.668 79.691,161.940 L79.691,153.262 C79.681,136.251 78.684,94.190 78.684,73.273 L78.684,73.112 C78.684,52.954 77.877,34.848 77.000,26.044 L77.000,10.992 C80.082,13.721 82.336,17.183 83.037,21.601 C84.106,28.343 84.761,49.460 85.214,73.413 C85.224,74.164 85.184,74.996 85.194,75.757 C85.627,99.720 85.738,143.424 85.738,160.024 L85.738,160.765 C85.738,160.775 85.778,160.785 85.778,160.795 C85.778,160.935 85.738,161.086 85.738,161.226 L85.738,163.510 C85.582,163.859 85.426,164.161 85.270,164.477 C85.327,164.879 85.376,165.294 85.422,165.712 C84.499,167.611 83.549,169.511 82.543,171.411 Z'
               ></path>
-            </clipPath>
-          </defs>
-          <path
-            fill-rule="evenodd"
-            fill="rgb(192, 127, 69)"
-            d="M82.543,171.411 C70.045,195.000 45.000,191.813 45.000,191.813 C45.000,191.813 19.955,195.000 7.456,171.411 C5.427,167.581 3.586,163.748 1.893,159.938 C1.940,159.800 1.981,159.649 2.030,159.514 C2.020,159.489 2.011,159.463 2.002,159.440 C2.022,159.159 2.052,158.869 2.092,158.578 L2.092,151.566 C2.113,134.435 2.092,95.372 3.100,73.483 L3.100,73.373 C3.100,49.470 3.785,28.333 4.854,21.601 C5.570,17.114 7.864,13.607 11.000,10.859 L11.000,23.822 C10.052,31.532 9.147,51.143 9.147,73.112 L9.147,73.263 C8.145,97.794 8.149,140.076 8.139,157.446 C9.254,171.519 17.668,185.000 44.906,185.000 C45.559,185.000 46.172,185.012 46.768,185.029 C68.800,184.025 77.362,173.668 79.691,161.940 L79.691,153.262 C79.681,136.251 78.684,94.190 78.684,73.273 L78.684,73.112 C78.684,52.954 77.877,34.848 77.000,26.044 L77.000,10.992 C80.082,13.721 82.336,17.183 83.037,21.601 C84.106,28.343 84.761,49.460 85.214,73.413 C85.224,74.164 85.184,74.996 85.194,75.757 C85.627,99.720 85.738,143.424 85.738,160.024 L85.738,160.765 C85.738,160.775 85.778,160.785 85.778,160.795 C85.778,160.935 85.738,161.086 85.738,161.226 L85.738,163.510 C85.582,163.859 85.426,164.161 85.270,164.477 C85.327,164.879 85.376,165.294 85.422,165.712 C84.499,167.611 83.549,169.511 82.543,171.411 Z"
-          ></path>
-        </svg>
-      </div>
-    </div>
-        <div class="espresso-cup-highlights"></div>
-        <sprite-animator
-          :id="'coffee-pour-animation'"
-          :spritesheet="require('../assets/coffee-pour@2x.png')"
-          :json="require('../assets/animation-data/coffee-pour-data.json')"
-          :yoyo="false"
-          :fps="30"
-          ref="coffeePourAnim"
-          :autoplay="false"
-        ></sprite-animator>
+            </svg>
+          </div>
+        </div>
+        <div class='espresso-cup-highlights'></div>
+        <vue-spritecore
+          v-bind:id="'coffee-pour-animation'"
+          v-bind:spritesheet="require('../assets/coffee-pour@2x.png')"
+          v-bind:json="require('../assets/animation-data/coffee-pour-data.json')"
+          v-bind:autoplay='false'
+          v-bind:loop='false'
+          v-bind:scaleX='0.833'
+          v-bind:scaleY='0.833'
+          v-bind:lowerBound='0'
+          v-bind:upperBound='54'
+          v-on:ready='pourAnimationReady'
+          v-on:animationStarted='pourAnimationStarted(...arguments)'
+          v-on:animationStopped='pourAnimationStopped(...arguments)'
+          v-on:animationReset='pourAnimationReset(...arguments)'
+          v-on:animationOver='pourAnimationOver(...arguments)'
+          ref='coffeePourAnimation'
+        ></vue-spritecore>
       </div>
     </div>
   </div>
@@ -54,12 +63,10 @@
 
 
 <script>
-import SpriteAnimator from "./sprite-animator";
-
+import SpriteAnimator from './sprite-animator';
 
 export default {
-  
-  name: "PourAnimation",
+  name: 'PourAnimation',
   data() {
     return {
       dragHandling: {
@@ -73,24 +80,22 @@ export default {
       coffeeWrapperRender: true
     };
   },
-  components: {
-    "sprite-animator": SpriteAnimator
-  },
+  components: {},
   mounted: function() {
     this.$anime({
       targets: this.$refs.groupHead,
       top: -30,
-      easing: "easeOutElastic(1, 0.7)",
+      easing: 'easeOutElastic(1, 0.7)',
       delay: 2200,
       duration: 1500,
       autoplay: true,
       loop: false
     });
     this.$anime({
-      targets: ".espresso-cup-wrapper",
+      targets: '.espresso-cup-wrapper',
       right: -80,
       rotateZ: 0,
-      easing: "easeOutElastic(1, 0.5)",
+      easing: 'easeOutElastic(1, 0.5)',
       delay: 2400,
       duration: 1500,
       autoplay: true,
@@ -106,16 +111,14 @@ export default {
       ) {
         //scale anim
         this.$anime({
-          targets: ".espresso-cup-wrapper",
+          targets: '.espresso-cup-wrapper',
           scale: 1.2,
-          easing: "easeOutElastic(1, 0.5)",
+          easing: 'easeOutElastic(1, 0.5)',
           delay: 0,
           duration: 800,
           autoplay: true,
           loop: false,
-          complete: function(anim){
-
-          }
+          complete: function(anim) {}
         });
 
         this.dragHandling.dragAllowed = true;
@@ -129,15 +132,23 @@ export default {
         this.dragHandling.dragInitialX = this.dragCurrentX;
         this.dragHandling.dragActive = false;
 
-        this.$anime.remove(".espresso-cup-wrapper");
+        this.$anime.remove('.espresso-cup-wrapper');
         console.log(
           this.$refs.espressoCupWrapper.style.right.substr(
             0,
-            this.$refs.espressoCupWrapper.style.right.indexOf("p")
+            this.$refs.espressoCupWrapper.style.right.indexOf('p')
           )
         );
-        if (this.$refs.espressoCupWrapper.style.right.substr(0,this.$refs.espressoCupWrapper.style.right.indexOf("p")) > 135 ||
-        this.$refs.espressoCupWrapper.style.right.substr(0,this.$refs.espressoCupWrapper.style.right.indexOf("p")) < 100) {
+        if (
+          this.$refs.espressoCupWrapper.style.right.substr(
+            0,
+            this.$refs.espressoCupWrapper.style.right.indexOf('p')
+          ) > 135 ||
+          this.$refs.espressoCupWrapper.style.right.substr(
+            0,
+            this.$refs.espressoCupWrapper.style.right.indexOf('p')
+          ) < 100
+        ) {
           //disable interaction
           this.dragHandling.dragAllowed = false;
           //since we don't dragend - reset posx
@@ -146,100 +157,41 @@ export default {
           this.dragHandling.dragXOffset = 80;
           //scale anim
           this.$anime({
-            targets: ".espresso-cup-wrapper",
+            targets: '.espresso-cup-wrapper',
             right: 135,
-            easing: "easeOutElastic(1, 0.5)",
+            easing: 'easeOutElastic(1, 0.5)',
             delay: 0,
             duration: 1000,
             autoplay: true,
             loop: false
           });
 
-          let ref = this;
+          //drip-pour anim
+          this.coffeeDripAnimation();
           //animate full-cup pour
-          this.coffeCupAnim(1000);
-         /* setTimeout(function(){
-            ref.$refs.coffeePourAnim.playOnce();
-             //move to the right
-          ref.$anime({
-            targets: ".espresso-cup-wrapper",
-            right: [
-              { value: 430, easing: "easeOutElastic(1, 0.4)", duration: 4000 }
-            ],
-            opacity: [
-              { value: 1, easing: "linear", duration: 0 },
-              { value: 0, easing: "linear" }
-            ],
-            rotateZ: [{ value: -10, duration: 200 }, { value: 0 }],
-            delay: 2300,
-            autoplay: true,
-            loop: false,
-            complete: function(){
-              //reset pour animation
-              ref.coffeeWrapperRender = false;
-             //ref.$refs.coffeePourAnim.reset();
-            }
+          this.sleep(1000).then(() => {
+            this.$refs.coffeePourAnimation.play();
+            this.sleep(2700).then( ()=>{
+              console.log('done');
+              this.$anime.remove('.espresso-cup-wrapper');
+              this.$refs.espressoCupWrapper.style.right = '-160px';
+              this.$refs.coffeePourAnimation.reset(0);
+              this.coffeeCupEntryAnimation();
+            })
+
           });
-          },1000)*/
-          
+
           //emit event on bus
           //bus.$emit('coffeIncrease');
-          this.$store.dispatch("increment");
-
-          //drip-pour anim
-          this.$anime
-          .timeline()
-          .add({
-            targets: "#drip-clip-path",
-            translateY: [-270, 10],
-            easing: "easeInOutQuint",
-            delay: 200,
-            duration: 1250,
-            autoplay: true,
-            loop: false
-          })
-          .add({
-            targets: "#drip-clip-path",
-            translateY: 195,
-            easing: "easeInOutQuint",
-            duration: 1300,
-            autoplay: true,
-            loop: false
-          }, 1200)
-           
-          //
-           setTimeout(function() {
-            //reset glass animation
-            let renderResetPromise = ref.coffeCupRendererReset();
-            renderResetPromise.then(rsp => {
-              ref.$refs.espressoCupWrapper.style.right = "-160px";
-              ref.$refs.espressoCupWrapper.style.opacity = 1;
-              ref.$anime.remove(".espresso-cup-wrapper");
-
-              //empty glass
-              ref.$anime({
-                targets: ".espresso-cup-wrapper",
-                right: -80,
-                rotateZ: 0,
-                easing: "easeOutElastic(1, 0.5)",
-                delay: 300,
-                duration: 1500,
-                autoplay: true,
-                loop: false,
-                complete: function(anim) {
-                  ref.dragHandling.dragAllowed = true;
-                }
-              });
-            });
-          }, 3700);
+          this.$store.dispatch('increment');
         }
 
         //scale anim
         this.$anime({
-          targets: ".espresso-cup-wrapper",
+          targets: '.espresso-cup-wrapper',
           scale: 1,
           rotateZ: 0,
-          easing: "easeOutElastic(1, 0.5)",
+          easing: 'easeOutElastic(1, 0.5)',
           delay: 0,
           duration: 800,
           autoplay: true,
@@ -258,9 +210,9 @@ export default {
           //rotate drag
           //scale anim
           this.$anime({
-            targets: ".espresso-cup-wrapper",
+            targets: '.espresso-cup-wrapper',
             rotateZ: -10,
-            easing: "easeOutElastic(1, 0.5)",
+            easing: 'easeOutElastic(1, 0.5)',
             delay: 0,
             duration: 800,
             autoplay: true,
@@ -271,58 +223,105 @@ export default {
         if (
           this.$refs.espressoCupWrapper.style.right.substr(
             0,
-            this.$refs.espressoCupWrapper.style.right.indexOf("p")
+            this.$refs.espressoCupWrapper.style.right.indexOf('p')
           ) < 220
         ) {
-          console.log("triggered dx: " + dx);
+          console.log('triggered dx: ' + dx);
 
-          this.$refs.espressoCupWrapper.style.right = -dx + "px";
+          this.$refs.espressoCupWrapper.style.right = -dx + 'px';
           //this.$refs.espressoCupWrapper.style.right = '300px';
           this.dragHandling.dragXOffset = this.dragHandling.dragCurrentX;
         }
       }
     },
-    coffeCupAnim: function(delay) {
+    pourAnimationReady: function() {
+      console.log('ready');
+      return new Promise((resolve, reject) => {
+        resolve(true);
+      });
+    },
+    pourAnimationStarted: function(startFrame, stopFrame) {
+      console.log('animation started:[' + startFrame + ',' + stopFrame + ']');
+    },
+    pourAnimationStopped: function(frame) {
+      console.log('animation stopped at frame: ' + frame);
+    },
+    pourAnimationReset: function(frame) {
+      console.log('animation resetted at frame: ' + frame);
+    },
+    pourAnimationOver: function(frame) {
+      console.log('animation over at frame:' + frame);
+      this.sleep(1400).then(() => this.animateCupRight());
+    },
+    sleep: function(mills) {
+      return new Promise(resolve => setTimeout(resolve, mills));
+    },
+    animateCupRight: function() {
       let ref = this;
-      setTimeout(function() {
-        //start coffee-pour anim
-        ref.$refs.coffeePourAnim.play();
+      this.$anime({
+        targets: '.espresso-cup-wrapper',
+        right: [
+          { value: 430, easing: 'easeOutElastic(1, 0.4)', duration: 4000 }
+        ],
+        opacity: [
+          { value: 1, easing: 'linear', duration: 0 },
+          { value: 0, easing: 'linear' }
+        ],
+        rotateZ: [{ value: -10, duration: 200 }, { value: 0 }],
+        delay: 0,
+        autoplay: true,
+        loop: false,
+        complete: function() {
 
-        setTimeout(function() {
-          ref.$refs.coffeePourAnim.stop();
-          //move to the right
-          ref.$anime({
-            targets: ".espresso-cup-wrapper",
-            right: [
-              { value: 430, easing: "easeOutElastic(1, 0.4)", duration: 4000 }
-            ],
-            opacity: [
-              { value: 1, easing: "linear", duration: 0 },
-              { value: 0, easing: "linear" }
-            ],
-            rotateZ: [{ value: -10, duration: 200 }, { value: 0 }],
-            delay: 1000,
+        }
+      });
+    },
+    coffeeDripAnimation: function() {
+      this.$anime
+        .timeline()
+        .add({
+          targets: '#drip-clip-path',
+          translateY: [-270, 10],
+          easing: 'easeInOutQuint',
+          delay: 200,
+          duration: 1250,
+          autoplay: true,
+          loop: false
+        })
+        .add(
+          {
+            targets: '#drip-clip-path',
+            translateY: 195,
+            easing: 'easeInOutQuint',
+            duration: 1250,
             autoplay: true,
             loop: false
-          });
-        }, 1400);
-      }, delay);
+          },
+          1200
+        );
     },
-    coffeCupRendererReset: function(delay = 10) {
-      return new Promise((resolve, reject) => {
-        let ref = this;
-        this.coffeeWrapperRender = false;
-        setTimeout(function() {
-          ref.coffeeWrapperRender = true;
-          resolve(true);
-        }, 1);
+    coffeeCupEntryAnimation() {
+      let ref = this;
+      this.$anime({
+        targets: '.espresso-cup-wrapper',
+        right: -80,
+        rotateZ: 0,
+        opacity: [0,1],
+        easing: 'easeOutElastic(1, 0.5)',
+        delay: 300,
+        duration: 1500,
+        autoplay: true,
+        loop: false,
+        complete: function(anim) {
+          ref.dragHandling.dragAllowed = true;
+        }
       });
     }
   }
 };
 </script>
 
-<style scoped>
+<style>
 #pour-animation {
   position: relative;
   width: 100%;
@@ -358,7 +357,6 @@ export default {
   left: -46px;
   top: -56px;
   transform: scale(0.6);
-  
 }
 
 .espresso-cup-highlights {
@@ -370,25 +368,21 @@ export default {
   top: 0px;
   z-index: 3;
 }
-.vue-sprite {
-  position: absolute;
-  left: -105px;
-  bottom: 52px;
-  transform: scale(0.833);
-  left: -9px;
-  bottom: 67px;
-}
+
 .coffee-drip-svg-wrapper {
   position: absolute;
-  /*left: 162px;
-  top: 110px;
-  transform: scale(1.01);*/
-  clip-path: url("#drip-clip");
+
+  clip-path: url('#drip-clip');
   left: 63.4px;
   top: -51px;
   transform: scale(1.68);
 }
 #drip-clip-path {
   transform: translateY(-270px);
+}
+#coffee-pour-animation {
+  position: absolute;
+  bottom: 88px;
+  left: 10.5px;
 }
 </style>
